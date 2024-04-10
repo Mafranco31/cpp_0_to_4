80fcd6c2c87761f4190b3aa9b1ca76036acbbcfd
Mafranco31 <mat.francois31@gmail.com>
2024-04-10 16:11:20 +0200

    efzaokp

---
+++ cpp01/ex05/Harl.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HumanA.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mafranco <mafranco@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/08 17:13:53 by mafranco          #+#    #+#             */
+/*   Updated: 2024/04/08 18:18:39 by mafranco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+# include <iostream>
+# include <string>
+
+class	Harl {
+private:
+	void	debug( void );
+	void	info( void );
+	void	warning( void );
+	void	error( void );
+
+public:
+	Harl();
+	~Harl();
+	void complain( std::string level );
+};
+
+#endif
