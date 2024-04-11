a7a34929ac8eed5e1df5d80a268baf4c66fec634
Mathis Francois <mafranco@car14s3.42barcelona.com>
2024-04-11 20:38:47 +0200

    fweq

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -6,37 +6,47 @@
 /*   By: mafranco <mafranco@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 19:10:31 by mafranco          #+#    #+#             */
-/*   Updated: 2024/04/11 19:29:41 by mafranco         ###   ########.fr       */
+/*   Updated: 2024/04/11 20:38:36 by mafranco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-Fixed::Fixed ( void ) num(0) {
+Fixed::Fixed( void ) : num(0) {
 
 	std::cout << "Default constructor called" << std::endl;
 
 	return;
 }
 
-Fixed::Fixed ( const Fixed& f) num(f.num) {
+Fixed& Fixed::operator=( const Fixed& f) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->num = f.getRawBits();
+	return *this;
+}
+
+Fixed::Fixed( const Fixed& f) {
 
 	std::cout << "Copy constructor called" << std::endl;
 	
+	*this = f;
+
 	return;
 }
 
-~Fixed::Fixed ( void ) {
+Fixed::~Fixed ( void ) {
 
 	std::cout << "Destructor called" << std::endl;
 
 	return;
 }
 
-Fixed::& operator=( const Fixed& f) {
+int	Fixed::getRawBits( void ) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return ( this->num );
+}
 
-
-	std::cout << "Copy assignment operator called" << std::endl;
-
-	return *this;
+void	Fixed::setRawBits( int const row ) {
+	this->num = row;
+	std::cout << "setRawBits member function called" << std::endl;
 }
--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: mafranco <mafranco@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 19:10:24 by mafranco          #+#    #+#             */
-/*   Updated: 2024/04/11 19:29:42 by mafranco         ###   ########.fr       */
+/*   Updated: 2024/04/11 20:38:37 by mafranco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,16 @@
 class	Fixed {
 	private:
 		int	num;
-		const static int	cs_num = 8;
+		static const int	cs_num = 8;
 
 	public:
 		Fixed();
-		Fixed( Fixed& f);
+		Fixed( const Fixed& f);
 		~Fixed();
+		Fixed& operator= ( const Fixed& f);
 		int	getRawBits( void ) const;
 		void	setRawBits( int const raw );
 
-}
+};
 
 #endif
